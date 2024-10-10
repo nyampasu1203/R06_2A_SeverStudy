5a73e2aa6ac4af7c6b689ff3d16f8c7826c15d25
GM2130050\ydauser01 <musashi634mita@gmail.com>
2024-10-10 11:17:29 +0900

    課題ファイルをコミット

---
+++ z14/js/kadai1.js
@@ -0,0 +1,13 @@
+
+window.onabort = function(){
+
+    let btn = document.getElementById("button");
+
+    btn.onclick = functoin();{
+        let main = document.getElementById("main")
+
+        let html1 = "<p>さくらみこ</p>"
+        let html2 = "<img src=\"img/Sakura-Miko.png\">";
+        main.innerHTML = html1 . html2;
+    }
+}
